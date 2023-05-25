c7a3f9e45d4552caecb42ca2c4b0627c21d2b0e0
Graeme Paterson <ghpaterson@gmail.com>
2023-05-25 11:53:23 +0100

    hero page image and minor styling

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
-"use client";
+import NavBar from "@/components/navbar";
 
 export default function Home() {
   return (
     <main>
-      <section>
-        <div>
-          <h1>Mid-Century Chairs</h1>
+      <section className="w-full h-screen relative bg-hero-image bg-cover bg-top">
+        <NavBar />
+        <div className="flex justify-end">
+          <div className="flex justify-center items-center bg-purple-700 w-96 h-96 mt-48">
+            <div className=" flex justify-center bg-transparent w-52 border-solid border-white border-2 rounded-xl py-2 px-4 text-white">
+              <h1>Explore the Collection</h1>
+            </div>
+          </div>
         </div>
       </section>
     </main>
--- components/navbar.js
+++ components/navbar.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="w-screen bg-black text-white text-lg lg:text-2xl px-6 py-4">
+    <nav className="w-screen bg-black text-white text-lg lg:text-2xl px-6 py-10">
       <div className="flex justify-between">
-        <h1>Stuhl</h1>
+        <h1>Mzansi Collection</h1>
         <ul className="flex justify-end gap-4 ">
           <Link href="/" alt="home">
             Home
